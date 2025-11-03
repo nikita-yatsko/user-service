b0aa5bbf33451c7b8298dfb41d38ae3d9fd57938
nikit <nikitatayatsko308@gmail.com>
2025-11-03 22:56:35 +0300

    Initial commit

---
+++ src/main/java/com/user/servise/User/Servise/UserServiseApplication.java
@@ -0,0 +1,13 @@
+package com.user.servise.User.Servise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/user/servise/User/Servise/UserServiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.servise.User.Servise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
