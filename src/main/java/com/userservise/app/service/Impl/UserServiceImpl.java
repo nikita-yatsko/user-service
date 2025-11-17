063363d737bb2c77de1f517c6a6c4b5fe8665190
nikit <nikitatayatsko308@gmail.com>
2025-11-17 20:50:22 +0300

    final v1

--- src/main/java/com/userservise/app/service/Impl/UserServiceImpl.java
+++ src/main/java/com/userservise/app/service/Impl/UserServiceImpl.java
@@ -12,6 +12,7 @@ import com.userservise.app.model.exception.NotFoundException;
 import com.userservise.app.repository.UserRepository;
 import com.userservise.app.service.UserService;
 import com.userservise.app.utils.specifications.UserSpecifications;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
@@ -73,7 +74,7 @@ public class UserServiceImpl implements UserService {
             throw new DataExistException(ErrorMessage.EMAIL_ALREADY_EXISTS.getMessage(request.getEmail()));
 
         if (!checkCardsCount(user))
-            throw new InvalidDataException(ErrorMessage.USER_CANNOT_HAVE_MORE_THAN_5_CARDS.getMessage(request.getEmail()));
+            throw new InvalidDataException(ErrorMessage.USER_CANNOT_HAVE_MORE_THAN_5_CARDS.getMessage(user.getId()));
 
         userMapper.updateUser(request, user);
         User updatedUser = userRepository.save(user);
@@ -83,28 +84,28 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
-    @CacheEvict(value = "users", key = "#id")
-    public Boolean activateUser(Integer id) {
+    @CachePut(value = "users", key = "#id")
+    public UserDto activateUser(Integer id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new NotFoundException(ErrorMessage.USER_NOT_FOUND_BY_ID.getMessage(id)));
 
         user.setActive(ActiveStatus.ACTIVE);
         User updatedUser = userRepository.save(user);
 
-        return updatedUser.getActive().equals(ActiveStatus.ACTIVE);
+        return userMapper.toDto(updatedUser);
     }
 
     @Override
     @Transactional
-    @CacheEvict(value = "users", key = "#id")
-    public Boolean deactivateUser(Integer id) {
+    @CachePut(value = "users", key = "#id")
+    public UserDto deactivateUser(@NotNull Integer id) {
         User user = userRepository.findUserById(id)
                 .orElseThrow(() -> new NotFoundException(ErrorMessage.USER_NOT_FOUND_BY_ID.getMessage(id)));
 
         user.setActive(ActiveStatus.INACTIVE);
         User updatedUser = userRepository.save(user);
 
-        return updatedUser.getActive().equals(ActiveStatus.INACTIVE);
+        return userMapper.toDto(updatedUser);
     }
 
     @Override
