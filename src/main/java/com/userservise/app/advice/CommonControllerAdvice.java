b77af51cd70891370116c888d5cac59b5b20f8be
nikit <nikitatayatsko308@gmail.com>
2025-11-21 16:00:44 +0300

    final v1.3 add exception handler

--- src/main/java/com/userservise/app/advice/CommonControllerAdvice.java
+++ src/main/java/com/userservise/app/advice/CommonControllerAdvice.java
@@ -2,12 +2,17 @@ package com.userservise.app.advice;
 
 import com.userservise.app.model.exception.DataExistException;
 import com.userservise.app.model.exception.InvalidDataException;
+import com.userservise.app.model.exception.InvalidTokenException;
 import com.userservise.app.model.exception.NotFoundException;
 import com.userservise.app.model.response.ErrorResponse;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -44,6 +49,20 @@ public class CommonControllerAdvice {
         log.error(e.getMessage());
 
         return buildErrorResponse(e, HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler({ BadCredentialsException.class, UsernameNotFoundException.class, InvalidTokenException.class, ExpiredJwtException.class })
+    public ResponseEntity<ErrorResponse> handleAuthenticationErrors(Exception e, HttpServletRequest request) {
+        log.error(e.getMessage());
+
+        return buildErrorResponse(e, HttpStatus.UNAUTHORIZED, request);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErrorResponse> handleAccessDeniedException(AccessDeniedException e, HttpServletRequest request) {
+        log.error(e.getMessage());
+
+        return buildErrorResponse(e, HttpStatus.FORBIDDEN, request);
     }
 
     private ResponseEntity<ErrorResponse> buildErrorResponse (Exception e, HttpStatus status, HttpServletRequest request) {
