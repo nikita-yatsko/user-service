776eea143828e2ba8298809b8f9fd03742bbb75c
nikit <nikitatayatsko308@gmail.com>
2025-11-22 14:30:19 +0300

    v0.2 test working (local)

--- src/test/java/com/user/servise/User/Servise/integration/BaseIntegrationTest.java
+++ src/test/java/com/user/servise/User/Servise/integration/BaseIntegrationTest.java
@@ -1,12 +1,18 @@
 package com.user.servise.User.Servise.integration;
 
+import com.redis.testcontainers.RedisContainer;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.time.Duration;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Testcontainers
 public class BaseIntegrationTest {
 
@@ -29,4 +35,20 @@ public class BaseIntegrationTest {
         registry.add("spring.data.redis.host", redis::getHost);
         registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
     }
+
+//    @Container
+//    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+//
+//    @Container
+//    static RedisContainer redis = new RedisContainer("redis:7");
+//
+//    @DynamicPropertySource
+//    static void configureProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+//        registry.add("spring.datasource.username", postgres::getUsername);
+//        registry.add("spring.datasource.password", postgres::getPassword);
+//
+//        registry.add("spring.data.redis.host", redis::getHost);
+//        registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
+//    }
 }
