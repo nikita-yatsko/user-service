8797a675cd9807751d42893c2d0b28aa1e7e404e
nikit <nikitatayatsko308@gmail.com>
2026-01-11 16:12:14 +0300

    edit tests

--- src/test/java/com/user/servise/User/Servise/integration/UserControllerTest.java
+++ src/test/java/com/user/servise/User/Servise/integration/UserControllerTest.java
@@ -6,11 +6,15 @@ import com.userservise.app.model.dto.UserRequest;
 import com.userservise.app.model.entity.User;
 import com.userservise.app.model.enums.ActiveStatus;
 import com.userservise.app.repository.UserRepository;
+import com.userservise.app.security.model.CustomUserDetails;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -18,6 +22,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 
 @ActiveProfiles("test")
 @SpringBootTest(classes = UserServiceApplication.class)
@@ -38,7 +45,10 @@ public class UserControllerTest extends BaseIntegrationTest{
 
     @BeforeEach
     void setUp() {
+        userRepository.deleteAll();
+
         savedUser = new User();
+        savedUser.setUserId(1);
         savedUser.setName("Test");
         savedUser.setSurname("User");
         savedUser.setEmail("testUserMail@mail.com");
@@ -50,18 +60,25 @@ public class UserControllerTest extends BaseIntegrationTest{
 
 
     @Test
-    public void getUserById_200_Ok() throws Exception {
+    public void getUserByIdReturn200Ok() throws Exception {
         // Given:
         User user = savedUser;
+        CustomUserDetails principal = new CustomUserDetails(1L, "ADMIN");
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                principal,
+                null,
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))
+        );
 
         // When:
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders
-                .get("/api/user/" + user.getId())
+                .get("/api/user/" + user.getUserId())
+                .with(authentication(auth))
                 .accept(MediaType.APPLICATION_JSON));
         // Then:
         response.andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(user.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.userId").value(user.getUserId()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(user.getName()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(user.getSurname()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(user.getEmail()))
@@ -70,13 +87,20 @@ public class UserControllerTest extends BaseIntegrationTest{
     }
 
     @Test
-    public void getUserById_404_NotFound() throws Exception {
+    public void getUserByIdReturn404NotFound() throws Exception {
         // Given:
         int id = 999;
+        CustomUserDetails principal = new CustomUserDetails(1L, "ADMIN");
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                principal,
+                null,
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))
+        );
 
         // When:
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders
                 .get("/api/user/" + id)
+                .with(authentication(auth))
                 .accept(MediaType.APPLICATION_JSON));
 
         // Then:
@@ -84,13 +108,20 @@ public class UserControllerTest extends BaseIntegrationTest{
     }
 
     @Test
-    public void getAllUsers_200_Ok() throws Exception {
+    public void getAllUsersReturn200Ok() throws Exception {
         // Given:
         String firstName = "Test";
+        CustomUserDetails principal = new CustomUserDetails(1L, "ADMIN");
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                principal,
+                null,
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))
+        );
 
         // When:
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders
                 .get("/api/user/all")
+                .with(authentication(auth))
                 .param("firstName", firstName)
                 .accept(MediaType.APPLICATION_JSON));
 
@@ -102,9 +133,10 @@ public class UserControllerTest extends BaseIntegrationTest{
     }
 
     @Test
-    public void createUser_201_Ok() throws Exception {
+    public void createUserReturn201Ok() throws Exception {
         // Given:
         UserRequest request = new UserRequest();
+        request.setUserId(2);
         request.setName("Name");
         request.setSurname("Surname");
         request.setBirthDate(LocalDate.of(2000, 1, 1));
@@ -124,7 +156,7 @@ public class UserControllerTest extends BaseIntegrationTest{
     }
 
     @Test
-    public void updateUser_200_Ok() throws Exception {
+    public void updateUserReturn200Ok() throws Exception {
         // Given:
         UserRequest request = new UserRequest();
         request.setName(savedUser.getName());
@@ -132,15 +164,23 @@ public class UserControllerTest extends BaseIntegrationTest{
         request.setBirthDate(savedUser.getBirthDate());
         request.setEmail("NewEmail@mail.com");
 
+        CustomUserDetails principal = new CustomUserDetails(1L, "ADMIN");
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                principal,
+                null,
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))
+        );
+
         // When:
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders
-                .put("/api/user/update/" + savedUser.getId())
+                .put("/api/user/update/" + savedUser.getUserId())
+                .with(authentication(auth))
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)));
 
         // Then:
         response.andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(savedUser.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.userId").value(savedUser.getUserId()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(request.getName()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(request.getSurname()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(request.getEmail()))
@@ -148,19 +188,26 @@ public class UserControllerTest extends BaseIntegrationTest{
     }
 
     @Test
-    public void setActiveUser_200_Ok() throws Exception {
+    public void setActiveUserReturn200Ok() throws Exception {
         // Given:
         savedUser.setActive(ActiveStatus.INACTIVE);
         userRepository.saveAndFlush(savedUser);
+        CustomUserDetails principal = new CustomUserDetails(1L, "ADMIN");
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                principal,
+                null,
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))
+        );
 
         // When:
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders
-                .put("/api/user/{id}/active", savedUser.getId())
+                .put("/api/user/{id}/active", savedUser.getUserId())
+                .with(authentication(auth))
                 .accept(MediaType.APPLICATION_JSON));
 
         // Then:
         response.andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(savedUser.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.userId").value(savedUser.getUserId()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(savedUser.getName()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(savedUser.getSurname()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(savedUser.getEmail()))
@@ -169,18 +216,25 @@ public class UserControllerTest extends BaseIntegrationTest{
 
 
     @Test
-    public void setInactiveUser_200_Ok() throws Exception {
+    public void setInactiveUserReturn200Ok() throws Exception {
         // Given:
         savedUser.setActive(ActiveStatus.ACTIVE);
+        CustomUserDetails principal = new CustomUserDetails(1L, "ADMIN");
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                principal,
+                null,
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))
+        );
 
         // When:
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders
-                .put("/api/user/{id}/inactive", savedUser.getId())
+                .put("/api/user/{id}/inactive", savedUser.getUserId())
+                .with(authentication(auth))
                 .accept(MediaType.APPLICATION_JSON));
 
         // Then:
         response.andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(savedUser.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.userId").value(savedUser.getUserId()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(savedUser.getName()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.surname").value(savedUser.getSurname()))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(savedUser.getEmail()))
@@ -188,18 +242,25 @@ public class UserControllerTest extends BaseIntegrationTest{
     }
 
     @Test
-    public void deleteUserById_200_Ok() throws Exception {
+    public void deleteUserByIdReturn200Ok() throws Exception {
         // Given:
         User user = savedUser;
+        CustomUserDetails principal = new CustomUserDetails(1L, "ADMIN");
+        Authentication auth = new UsernamePasswordAuthenticationToken(
+                principal,
+                null,
+                List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))
+        );
 
         // When:
         ResultActions response = mockMvc.perform(MockMvcRequestBuilders
-                .delete("/api/user/{id}", user.getId()));
+                .delete("/api/user/{userId}", user.getUserId())
+                .with(authentication(auth)));
 
         // Then:
         response.andExpect(MockMvcResultMatchers.status().isOk());
 
-        boolean exists = userRepository.existsById(user.getId());
+        boolean exists = userRepository.existsById(user.getUserId());
         Assertions.assertFalse(exists, "User should be deleted from database");
     }
 }
